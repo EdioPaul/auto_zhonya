3112393c18afa112cc4640bb5bcf2310a5822177
EdioPaul <ediopaulo.ep@outlook.com>
2023-08-30 19:57:47 -0300

    Add health percent dinamic base life

--- auto_zhonya.py
+++ auto_zhonya.py
@@ -7,6 +7,9 @@ import keyboard
 def find_summoner_name(data):
     return data["activePlayer"]["summonerName"]
 
+def find_max_health(data):
+        return data["activePlayer"]["championStats"]["maxHealth"]
+
 
 def check_health(data, health_threshold):
     if data["activePlayer"]["championStats"]["currentHealth"] <= health_threshold:
@@ -22,17 +25,17 @@ def is_alive(data, player):
     return False
 
 
-def use_item(data, player, health_threshold):
+def use_item(data, health_threshold):
     if check_health(data, health_threshold):
-            print('\x1b[6;30;43m' + 'Zhonya activated      ' + '\x1b[0m')
+            print('Zhonya')
             keyboard.press(str(2))
             time.sleep(3)
-            print('\x1b[5;30;42m' + 'Spell Two F activated ' + '\x1b[0m')
+            print('Spell Two F')
             keyboard.press('F')
             time.sleep(1)
-            print('\x1b[2;30;44m' + 'Potion activated      ' + '\x1b[0m')
+            print('Potion')
             keyboard.press(str(1))
-            time.sleep(15)
+            
 
 cert_path = "LoL Game Engineering Certificate Authority.crt"
 
@@ -43,7 +46,6 @@ data = json.loads(response.text)
 
 player = find_summoner_name(data)
 
-health_threshold = 400
 
 while player:
     response = requests.get(
@@ -52,6 +54,11 @@ while player:
     data = json.loads(response.text)
 
     if is_alive(data, player):
-        use_item(data, player, health_threshold)
+        max_health = int(find_max_health(data))
+        print('Max Health  ', max_health)
+        percent_max_health = 35
+        health_threshold = int(max_health * percent_max_health / 100)
+        print('Zhonya apply ', health_threshold)
+        use_item(data, health_threshold)
 
     time.sleep(0.3)
